9d391219a279d2baa0cd9ffc48338b9aa363d490
kihno <stistyle13@gmail.com>
2022-08-22 12:18:51 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+import Header from './components/Header';
+import Work from './components/Work';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Work />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
