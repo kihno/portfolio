110ae3653ef81df10741d26f5c7b3d44e23e34c4
kihno <stistyle13@gmail.com>
2022-08-23 13:20:16 -0700

    Style header for mobile

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,7 +3,17 @@ import React from "react";
 const Work = () => {
 
     return(
-        <div>Work</div>
+        <div id="work">
+            <div className="techContainer">
+                <i class="devicon-html5-plain"></i>
+                <i class="devicon-css3-plain"></i>
+                <i class="devicon-javascript-plain"></i>
+                <i class="devicon-webpack-plain"></i>
+                <i class="devicon-react-original"></i>
+                <i class="devicon-firebase-plain"></i>
+                <i class="devicon-nodejs-plain"></i>
+            </div>
+        </div>
     )
 }
 
