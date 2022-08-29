1b4dcd0d1d19705096e0357870cb1822a5ada5ef
kihno <stistyle13@gmail.com>
2022-08-29 11:32:26 -0700

    Deploy gh-pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,8 +14,8 @@ const Footer = () => {
                 <p className="flex phone"><Icon className="custom-icon" path={mdiPhone} size={1} />818-268-2242</p>
                 <p className="flex email"><Icon className="custom-icon" path={mdiEmail} size={1} />nmiles13@@gmail.com</p>
                 <div className="iconContainer">
-                    <a href="https://github.com/kihno"><i className="devicon-github-original"></i></a>
-                    <a href="https://linkedin.com/in/nicholas-miles-93328227/"><i className="devicon-linkedin-plain"></i></a>
+                    <a href="https://github.com/kihno"><i className="icon devicon-github-original"></i></a>
+                    <a href="https://linkedin.com/in/nicholas-miles-93328227/"><i className="icon devicon-linkedin-plain"></i></a>
                 </div> 
             </div>
             <img className="footerImage" src={webdev} alt=""></img>
